bdc3e41b00a02df229e5f6e3c1cc346b315cc359
Andrew Katsewich <andrew.katsewich@targsoftware.com>
2020-11-18 17:07:44 +0300

    Added icon for mobile group activity

--- src/Components/Activity/Activity.tsx
+++ src/Components/Activity/Activity.tsx
@@ -7,9 +7,14 @@ const classnames = require("classnames");
 
 const Activity = ({ user, group }: any) => {
   return (
-    <div className={classnames(styles.sidebar_container, "desktop_only")}>
-      <Log user={user} group={group} />
-    </div>
+    <>
+      <div className={classnames(styles.sidebar_container, "desktop_only")}>
+        <Log user={user} group={group} />
+      </div>
+      <div className={classnames(styles.icons, "mobile_only")}>
+        <i className="far fa-comment"></i>
+      </div>
+    </>
   );
 };
 
