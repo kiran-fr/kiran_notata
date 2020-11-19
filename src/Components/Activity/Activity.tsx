a48d5673311540c64de5331cae1379411b63ee7b
Andrew Katsewich <andrew.katsewich@targsoftware.com>
2020-11-19 16:22:27 +0300

    Activity mobile flow

--- src/Components/Activity/Activity.tsx
+++ src/Components/Activity/Activity.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import styles from "./Activity.module.css";
 
 import { Log } from "./Log";
@@ -6,12 +6,40 @@ import { Log } from "./Log";
 const classnames = require("classnames");
 
 const Activity = ({ user, group }: any) => {
+  const [visibleMobile, setVisibleMobile] = useState(false);
+
+  const ref = useRef(null);
+  const clickListener = useCallback(
+    (e: MouseEvent) => {
+      if (!(ref.current! as any).contains(e.target)) setVisibleMobile(false);
+    },
+    [ref.current]
+  );
+
+  useEffect(() => {
+    document.addEventListener("mousedown", clickListener);
+    return () => {
+      document.removeEventListener("mousedown", clickListener);
+    };
+  }, []);
+
   return (
     <>
-      <div className={classnames(styles.sidebar_container, "desktop_only")}>
+      <div
+        ref={ref}
+        className={classnames(
+          styles.sidebar_container,
+          visibleMobile
+            ? styles.open_mobile_container
+            : styles.closed_mobile_container
+        )}
+      >
         <Log user={user} group={group} />
       </div>
-      <div className={classnames(styles.icons, "mobile_only")}>
+      <div
+        className={classnames(styles.icons, "mobile_only")}
+        onClick={() => setVisibleMobile(!visibleMobile)}
+      >
         <i className="far fa-comment"></i>
       </div>
     </>
