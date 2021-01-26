54a2d39e0a2394fcb6a2ed44b69c5af46fa1cad6
Andrew Katsewich <andrew.katsewich@targsoftware.com>
2021-01-26 14:20:20 +0300

    Added errors check for submit page

--- src/pages/public/EvaluationPage/Section.js
+++ src/pages/public/EvaluationPage/Section.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { useForm } from "react-hook-form";
 
-import { Card, Button, GhostLoader } from "Components/elements";
+import { Card, Button, ErrorBox, GhostLoader } from "Components/elements";
 
 import { publicEvaluationTemplateSectionGet } from "Apollo/Queries";
 
@@ -111,6 +111,20 @@ function SubmitForm({ setPersonalData }) {
   );
 }
 
+function checkErrors(answers, personalData) {
+  let errs = [];
+
+  if (answers.length === 0)
+    errs.push(
+      "You didn't provide any evaluations, please return to previous sections and fill in at least 1 question."
+    );
+
+  if (!personalData.firstname || !personalData.lastname || !personalData.email)
+    errs.push("Contact info is not provided.");
+
+  return errs;
+}
+
 export default function Section({
   sections,
   sectionId,
@@ -121,6 +135,8 @@ export default function Section({
 }) {
   const [isSubmit, setSubmit] = useState(false);
   const [personalData, setPersonalData] = useState({});
+  const [errors, setErrors] = useState([]);
+
   // Get evaluation template section
   const evaluationTemplateSectionQuery = useQuery(
     publicEvaluationTemplateSectionGet,
@@ -161,6 +177,14 @@ export default function Section({
     <>
       {currentForm}
 
+      {!!errors.length && isSubmit && (
+        <ErrorBox className="mb3">
+          {errors.map((err, i) => (
+            <div key={i}>{err}</div>
+          ))}
+        </ErrorBox>
+      )}
+
       <Navigation
         sections={sections}
         sectionId={sectionId}
@@ -168,6 +192,9 @@ export default function Section({
         setSubmit={setSubmit}
         isSubmit={isSubmit}
         submitEvaluation={() => {
+          const errors = checkErrors(answers, personalData);
+          setErrors(errors);
+          if (errors.length) return;
           submitEvaluation(personalData);
         }}
       />
