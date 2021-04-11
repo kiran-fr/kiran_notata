56a2a1c84555fde4923855a4ca74bc7b71f0d1fb
sivakumar <sivakumarjegadesan@gmail.com>
2021-04-12 01:25:23 +0530

    Drop menu changes.

--- src/Components/UI_Kits/DropMenu/Component/IconButton/IconButton.js
+++ src/Components/UI_Kits/DropMenu/Component/IconButton/IconButton.js
@@ -1,16 +1,41 @@
 import React, { useState } from "react";
 import "./style.css";
 
-function IconButton({ iconName, title }) {
-  const [isClicked, setClicked] = useState(false);
+function IconButton({ dropMenuArr }) {
+  const [isClicked, setClicked] = useState(null);
+
+  const markedItem = index => {
+    setClicked(index);
+  };
 
   return (
-    <div onClick={() => setClicked(!isClicked)} className="icon-btn-div">
-      <i className={isClicked ? `${iconName} i-after` : `${iconName} icon`} />
-      <span className={isClicked ? "click-btn-after" : "click-btn"}>
-        {title}
-      </span>
-    </div>
+    dropMenuArr.length &&
+    dropMenuArr.map((item, index) => (
+      <div
+        key={index}
+        onClick={() => markedItem(index)}
+        className="icon-btn-div"
+      >
+        <i
+          style={index === isClicked ? { color: "red" } : { color: "gray" }}
+          className={
+            index === isClicked
+              ? `${item.iconName} i-after`
+              : `${item.iconName} icon`
+          }
+        />
+        <span
+          style={
+            index === isClicked
+              ? { color: "red", marginLeft: "5px" }
+              : { color: "gray", marginLeft: "5px" }
+          }
+          // className={index === 1 ? "click-btn-after" : "click-btn"}
+        >
+          {item.title}
+        </span>
+      </div>
+    ))
   );
 }
 
--- src/Components/UI_Kits/DropMenu/DropMenu.js
+++ src/Components/UI_Kits/DropMenu/DropMenu.js
@@ -1,16 +1,14 @@
 import React from "react";
 import Card from "@material-ui/core/Card";
-import "./style.css";
 import { IconButton } from "./Component/IconButton";
+import "./style.css";
 
-const DropMenu = () => {
+function DropMenu({ dropMenuArr }) {
   return (
     <Card className="drop-menu-card">
-      <IconButton title="Request Startup" iconName="fa fa-briefcase" />
-      <IconButton title="Archive Startup" iconName="fa fa-briefcase" />
-      <IconButton title="Remove Startup" iconName="fa fa-briefcase" />
+      {dropMenuArr.length && <IconButton dropMenuArr={dropMenuArr} />}
     </Card>
   );
-};
+}
 
 export default DropMenu;
