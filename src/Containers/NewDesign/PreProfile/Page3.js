77cd56a52506ef5f02f549ac5d2aea8297d1d1c0
shirisha Eppakayala <shirisha.eppakayala8@gmail.com>
2021-04-19 11:34:00 +0200

    redirecting to dashboard

--- src/Containers/NewDesign/PreProfile/Page3.js
+++ src/Containers/NewDesign/PreProfile/Page3.js
@@ -1,17 +1,43 @@
 /* Date : 10/04/2020 */
 /* Created By : siva */
 
-import React from "react";
+import React, { useState } from "react";
 
 // REACT STUFF
 import { useForm } from "react-hook-form";
 
+// API STUFF
+import { useMutation } from "@apollo/client";
+import { userUpdate } from "private/Apollo/Mutations";
+import { dashboard } from "definitions.js";
+
 import styles from "./Profile.module.css";
 import Group from "./group/group";
+import { Button } from "Components/UI_Kits";
 
-export default function Page3({ setPage, extraInputs, setExtraInputs }) {
+export default function Page3({ extraInputs, history }) {
+  const [mutate] = useMutation(userUpdate);
+  const [loading, setLoading] = useState(false);
   const { register, handleSubmit, formState } = useForm();
   const { isSubmitting } = formState;
+  const onSubmit = async (data, event) => {
+    event.preventDefault();
+
+    let input = {
+      ...extraInputs,
+      /* investor: data.input.investor, */
+    };
+
+    try {
+      setLoading(true);
+      await mutate({ variables: { input } });
+    } catch (error) {
+      console.log("error", error);
+    }
+    setLoading(false);
+
+    history.push(dashboard);
+  };
 
   const data = [
     { title: "business angels 1", admin: "Stephanie Wykoff" },
@@ -20,11 +46,11 @@ export default function Page3({ setPage, extraInputs, setExtraInputs }) {
     { title: "business angels 4", admin: "Stephanie Wykoff" },
   ];
 
-  const onSubmit = (data, event) => {
-    event.preventDefault();
-  };
+  // const onSubmit = (data, event) => {
+  //   event.preventDefault();
+  // };
 
-  function handleKeyDown(e) {
+  /* function handleKeyDown(e) {
     if (e.key === "Enter" || e.keyCode === 188) {
       let val = e.target.value;
       if (val === "") return;
@@ -41,21 +67,33 @@ export default function Page3({ setPage, extraInputs, setExtraInputs }) {
     if (val === ",") {
       e.target.value = "";
     }
-  }
+  } */
 
   return (
-    <div className={styles.profile3}>
-      <h1
-        style={{
-          marginBottom: "50px",
-        }}
-      >
-        Do you want to be a part of these groups?
-      </h1>
+    <form
+      className="notata_form"
+      onSubmit={handleSubmit(onSubmit)}
+      style={{ marginBottom: "20px" }}
+    >
+      <div className={styles.profile3}>
+        <h1
+          style={{
+            marginBottom: "50px",
+          }}
+        >
+          Do you want to be a part of these groups?
+        </h1>
 
-      {data.map((item, i) => (
-        <Group key={i} title={item.title} admin={item.admin} />
-      ))}
-    </div>
+        {data.map((item, i) => (
+          <Group key={i} title={item.title} admin={item.admin} />
+        ))}
+        <Button
+          type="input"
+          // value={isDirty ? "SAVE" : "SKIP"}
+          value={"NEXT"}
+          loading={loading}
+        />
+      </div>
+    </form>
   );
 }
