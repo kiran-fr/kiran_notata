0424121fac385b30153c0526c01ba82788e8f601
siva <sivakumarjegadesan@gmail.com>
2021-05-12 20:50:09 +0530

    merge issue resolved.

--- src/private/NewDesign/Startup/Filters.js
+++ src/private/NewDesign/Startup/Filters.js
@@ -1,15 +1,4 @@
 import React, { useState, useEffect } from "react";
-<<<<<<< HEAD
-import TagSelector from "Components/TagSelector/TagSelector";
-import { Tag, Modal } from "Components/elements/";
-// import DateRangeSelector from "Components/elements/NotataComponents/DateRangeSelector";
-
-import { funnelGroupGet, tagGroupsGet } from "private/Apollo/Queries";
-import { useQuery } from "@apollo/client";
-import { cloneDeep } from "lodash";
-import classnames from "classnames";
-=======
->>>>>>> 07405f0f9cc334477681b1af4a62d7f253eac4f1
 import styles from "./filter.module.css";
 import Filterr from "../../../assets/images/filter.png";
 import Column from "../../../assets/images/column.png";
@@ -18,89 +7,12 @@ import AddStartup from "./Modal/addStartup";
 import { Tabsection } from "Components/UI_Kits/Tabs/index";
 import FilterSidebar from "Components/secondarySidebar/filter";
 import ColumnSidebar from "Components/secondarySidebar/manage";
-<<<<<<< HEAD
-
-import PopupMenu from "./PopupMenu";
-
-import moment from "moment";
-=======
->>>>>>> 07405f0f9cc334477681b1af4a62d7f253eac4f1
 import {
   container,
   container_mini,
   footer,
-<<<<<<< HEAD
-  // filter_star,
-  filter_icon_container,
-  filter_icon,
-  tag_each,
-  tag_kill,
-  filter_container,
-  // filter_content,
-  funnel_tag_container,
-  funnel_tag,
-  funnel_tag_active,
-} from "./Filters.module.scss";
-// import {
-//   // clear_filters,
-//   // counter,
-//   // small_text_flex,
-// } from "./Connections.module.css";
-
-const Tags = ({ filters, tagGroups, setFilters }) => {
-  const [show, setShow] = useState(false);
-
-  return (
-    <div className={filter_icon_container}>
-      <div className={filter_icon} onClick={() => setShow(!show)}>
-        <i className={`${filters.tags?.length ? "fas" : "fal"} fa-tag`} />
-      </div>
-
-      <TagSelector
-        show={show}
-        close={() => setShow(false)}
-        tagGroups={tagGroups}
-        checkedTags={filters.tags}
-        addTag={tag => {
-          setFilters({
-            ...filters,
-            tags: [...filters.tags, tag],
-          });
-        }}
-        deleteTag={tag => {
-          setFilters({
-            ...filters,
-            tags: filters.tags.filter(({ id }) => id !== tag.id),
-          });
-        }}
-      />
-    </div>
-  );
-};
-
-const Funnels = ({ filters, setFilters, setFilterType }) => {
-  const { data, error, loading } = useQuery(funnelGroupGet);
-
-  const [show, setShow] = useState(false);
-
-  let funnelGroups = [];
-  if (data && !error && !loading) {
-    funnelGroups = data.accountGet.funnelGroups;
-  }
-
-  const addFunnelTag = funnelTag => {
-    // let group = funnelGroups.find(({ id }) => funnelTag.funnelGroupId === id);
-
-    if (filters.funnelTags.some(({ id }) => id === funnelTag.id)) {
-      return setFilters([]);
-    }
-
-    setFilters([funnelTag]);
-  };
-=======
   filter_container,
 } from "./Filters.module.scss";
->>>>>>> 07405f0f9cc334477681b1af4a62d7f253eac4f1
 
 const OptionalFilterSidebar = ({ setOptionalFilter }) => {
   return (
@@ -124,11 +36,6 @@ const OptionalFilterSidebar = ({ setOptionalFilter }) => {
   );
 };
 
-<<<<<<< HEAD
-function getHasFilters(filters) {
-  return false;
-}
-=======
 const tabArr = [
   {
     value: "kanban",
@@ -149,7 +56,6 @@ const tabArr = [
     ),
   },
 ];
->>>>>>> 07405f0f9cc334477681b1af4a62d7f253eac4f1
 
 export default function Filters({
   history,
@@ -177,74 +83,9 @@ export default function Filters({
     setFilterValue(filters.search);
   }, [filters]);
 
-<<<<<<< HEAD
-  const tabArr = [
-    {
-      value: "kanban",
-      text: (
-        <div>
-          <img
-            style={{
-              width: 15,
-              height: 15,
-              marginRight: "4px",
-              transform: "rotateZ(360deg)",
-            }}
-            src={KanbanIcon}
-            alt=""
-          />
-          <span>KANBAN</span>
-          <i
-            onClick={() => setKanbanPopup(!kanbanPopup)}
-            style={{ marginLeft: "5px" }}
-            className="fas fa-chevron-down"
-          ></i>
-          <PopupMenu
-            title="Kanban"
-            items={["All Startups", "Funnel 1", "Funnel 2", "Funnel 3"]}
-            isOpen={kanbanPopup}
-            setIsOpen={setKanbanPopup}
-          ></PopupMenu>
-          {/* <DropMenu
-            dropMenuArr={[
-              { iconName: "", title: "Funnel 1" },
-              { iconName: "", title: "Funnel 2" },
-              { iconName: "", title: "Funnel 3" },
-              { iconName: "", title: "Funnel 3" },
-              { iconName: "", title: "Funnel 3" },
-              { iconName: "", title: "Funnel 3" },
-            ]}
-          ></DropMenu> */}
-        </div>
-      ),
-    },
-    {
-      value: "spreadsheet",
-      text: (
-        <div>
-          <img
-            style={{ width: 15, height: 15, marginRight: "4px" }}
-            src={Column}
-            alt=""
-          />
-          <span>SPREADSHEET</span>
-        </div>
-      ),
-    },
-  ];
-
-  const formatDateTag = range => {
-    let result = "";
-    if (range[0]) {
-      result += ` From ${moment(range[0]).format("MM-DD-YYYY")}`;
-    }
-    if (range[1]) {
-      result += ` To ${moment(range[1]).format("MM-DD-YYYY")}`;
-=======
   useEffect(() => {
     if (filterValue === "") {
       setFilters({ ...filters, search: filterValue });
->>>>>>> 07405f0f9cc334477681b1af4a62d7f253eac4f1
     }
   }, [filterValue]);
 
