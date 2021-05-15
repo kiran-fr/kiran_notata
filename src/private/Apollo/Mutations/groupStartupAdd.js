2ca3192bf9030fe991e1b598444897fd3239b793
Amruth L S <lsamruth@gmail.com>
2021-05-15 22:16:12 +0530

    add group tag to connection on on-boarding

--- src/private/Apollo/Mutations/groupStartupAdd.js
+++ src/private/Apollo/Mutations/groupStartupAdd.js
@@ -2,6 +2,9 @@ import gql from "graphql-tag";
 
 export default gql`
   mutation groupStartupAdd($groupId: ID!, $creativeId: ID!) {
-    groupStartupAdd(groupId: $groupId, creativeId: $creativeId)
+    groupStartupAdd(groupId: $groupId, creativeId: $creativeId) {
+      id
+      name
+    }
   }
 `;
