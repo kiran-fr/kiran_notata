d2d958df3ea3f129b838ffd183240b55f3255d59
sgoyal2243 <shubhamgoyal2243@gmail.com>
2021-05-22 03:22:26 -0700

    group dashboard

--- src/Components/UI_Kits/Modal/Modal.js
+++ src/Components/UI_Kits/Modal/Modal.js
@@ -46,7 +46,7 @@ export function Modal({
           }
         }}
       >
-        <div className={`inner ${innerClassName}`}>
+        <div className={`inner ${innerClassName || ""}`}>
           {title && (
             <div className={"modal_header"}>
               {title && <div className={"modal_title"}>{title}</div>}
