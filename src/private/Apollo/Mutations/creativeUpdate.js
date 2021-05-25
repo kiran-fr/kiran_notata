919758f83b92189ebb9621968fee1a82ac015a2f
atatonotata <jorgen@ataton.no>
2021-05-25 16:57:51 +0200

    logo uploader

--- src/private/Apollo/Mutations/creativeUpdate.js
+++ src/private/Apollo/Mutations/creativeUpdate.js
@@ -3,7 +3,7 @@ import { creativeFragments } from "Apollo/Fragments";
 
 export default gql`
   mutation creativeUpdate($id: ID!, $input: CreativeInput!) {
-    creativePut(id: $id, input: $input) {
+    creativeUpdate(id: $id, input: $input) {
       ...creativeFields
     }
   }
