dc70c7f6d0894939fa95910ff1e0babd3f69352e
siva <sivakumarjegadesan@gmail.com>
2021-06-02 09:24:45 +0530

    date picker logic.

--- src/Components/UI_Kits/DatePicker/datepicker1.js
+++ src/Components/UI_Kits/DatePicker/datepicker1.js
@@ -1,9 +1,59 @@
 import React, { useState } from "react";
 import "./datepicker.scss";
 import DatePicker from "react-datepicker";
+import moment from "moment";
 import "react-datepicker/dist/react-datepicker.css";
 
+
+
 export const  Datepicker1 = ({ isCalendarVisible, setCalendarVisible, selected, onChange, startDate, endDate, formatWeekDay }) => {
+  
+  const [daterange, setDaterange] = useState([null, null]);
+
+  const setRange = (range) => {
+    if (range[1]) {
+      range[1] = moment(range[1]).endOf("day").toDate();
+    }
+    setDaterange(range);
+    onChange("dateRange", range );
+  };
+
+  const presetDateValues = () => {
+    const toDate = moment().endOf("day").toDate();
+    const selectDateRange = [
+      {
+        label: "Last 7 Days",
+        range: [moment().subtract(7, "day").startOf("day").toDate(), toDate],
+      },
+      {
+        label: "Last 14 Days",
+        range: [moment().subtract(14, "day").startOf("day").toDate(), toDate],
+      },
+      {
+        label: "Last 30 Days",
+        range: [moment().subtract(30, "day").startOf("day").toDate(), toDate],
+      },
+      {
+        label: "Last 90 Days",
+        range: [moment().subtract(90, "day").startOf("day").toDate(), toDate],
+      },
+      {
+        label: "Last Year",
+        range: [moment().startOf("year").toDate(), toDate],
+      },
+    ];
+    return selectDateRange.map((value, index) => {
+      return (
+        <div key={index}
+          onClick={() => setRange(value.range)}
+          className="datepicker-container__last-day-container__last-day"
+        >
+          {value.label}
+        </div>
+      );
+    });
+  }
+  
   return (
     <>
       <div className="datepicker-container ">
@@ -36,21 +86,7 @@ export const  Datepicker1 = ({ isCalendarVisible, setCalendarVisible, selected, 
           </div>
         </div>
         <div className="datepicker-container__last-day-container">
-          <div className="datepicker-container__last-day-container__last-day">
-            last 7 days
-          </div>
-          <div className="datepicker-container__last-day-container__last-day">
-            last 14 days
-          </div>
-          <div className="datepicker-container__last-day-container__last-day">
-            last 30 days
-          </div>
-          <div className="datepicker-container__last-day-container__last-day">
-            last 90 days
-          </div>
-          <div className="datepicker-container__last-day-container__last-day">
-            last year
-          </div>
+          {presetDateValues()}
         </div>
         {isCalendarVisible && (
           <>
--- src/Components/secondarySidebar/filter.js
+++ src/Components/secondarySidebar/filter.js
@@ -13,85 +13,12 @@ import { CheckBox, Datepicker1 } from "Components/UI_Kits";
 // API
 import { useQuery } from "@apollo/client";
 import { funnelGroupGet } from "private/Apollo/Queries";
-import DateRangeSelector from "Components/elements/NotataComponents/DateRangeSelector";
 import moment from "moment";
 
-import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import styles from "./sidebar.module.scss";
 
 import More from "assets/images/more.svg";
-
-// const DatePicker = ({ filters, setFilters }) => {
-//   const setDateFilter = dateRange => {
-//     let from = moment(dateRange[0]);
-//     let to = moment(dateRange[1]);
-
-//     if (from.isValid() && to.isValid()) {
-//       let fromDate = from?.format("x");
-//       let toDate = to?.format("x");
-//       console.log(fromDate, toDate);
-//       setFilters({ ...filters, fromDate, toDate });
-//     }
-//   };
-
-//   return (
-//     <DateRangeSelector
-//       value={[filters?.fromDate, filters?.toDate]}
-//       onValueChange={setDateFilter}
-//     />
-//   );
-// };
-
-// const DatePickerNewdesign = ({ filters, setFilters }) => {
-//   const [fromDate, setFromDate] = useState(null);
-//   const [toDate, setToDate] = useState(null);
-
-//   const handleChange = e => {
-//     let { name, value } = e.target;
-//     value = moment(value).format("X");
-//     setFilters({ ...filters, [name]: value });
-//   };
-
-//   useEffect(() => {
-//     if (moment(filters.fromDate)?.isValid()) {
-//       setFromDate(moment(filters.fromDate).format("YYYY-MM-DD"));
-//     } else {
-//       setFromDate(moment.unix(filters.fromDate).format("YYYY-MM-DD"));
-//     }
-//   }, [filters.fromDate]);
-
-//   useEffect(() => {
-//     if (moment(filters.toDate)?.isValid()) {
-//       setToDate(moment(filters.toDate)?.format("YYYY-MM-DD"));
-//     } else {
-//       setToDate(moment.unix(filters.toDate)?.format("YYYY-MM-DD"));
-//     }
-//   }, [filters.toDate]);
-
-//   return (
-//     <div className={styles.dateStage}>
-//       <div className={styles.dateFrom}>
-//         <label>From</label>
-//         <input
-//           type="date"
-//           name="fromDate"
-//           value={fromDate}
-//           onChange={handleChange}
-//         />
-//       </div>
-//       <div className={styles.dateTo}>
-//         <label>To</label>
-//         <input
-//           type="date"
-//           name="toDate"
-//           value={toDate}
-//           onChange={handleChange}
-//         />
-//       </div>
-//     </div>
-//   );
-// };
 
 export default function FilterBar({
   close,
@@ -108,35 +35,38 @@ export default function FilterBar({
 
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
-  const onChange = dates => {
-    const [start, end] = dates;
 
-    // let fromDate = start?.format("x");
-    // let toDate = end?.format("x");
-    // setFilters({ ...filters, fromDate, toDate });
-    setStartDate(start);
-    setEndDate(end);
-
-    if (start && end) {
-      setCalendarVisible(false);
+  const onChange = (dates, range) => {
+    if(dates === "dateRange") {
+      if (range) {
+        let fromDate = range[0] ? moment(range[0]).format("x") : "";
+        let toDate = range[1] ? moment(range[1]).format("x") : "";
+        if(fromDate && toDate ) {
+          setFilters({ ...filters, fromDate, toDate });
+        }
+        setStartDate(range[0]);
+        setEndDate(range[1]);
+        if (fromDate && toDate) {
+          setCalendarVisible(false);
+      }
+    } else {
+        const [start, end] = dates;
+        let fromDate = start ? moment(start).format("x") : "";
+        let toDate = end ? moment(end).format("x") : "";
+    
+        if(fromDate && toDate ) {
+          setFilters({ ...filters, fromDate, toDate });
+        }
+        setStartDate(start);
+        setEndDate(end);
+    
+        if (start && end) {
+          setCalendarVisible(false);
+        }
+      }
     }
-  };
+  }
 
-  // useEffect(() => {
-  //   if (moment(filters.fromDate)?.isValid()) {
-  //     setStartDate(moment(filters.fromDate).format("YYYY-MM-DD"));
-  //   } else {
-  //     setStartDate(moment.unix(filters.fromDate).format("YYYY-MM-DD"));
-  //   }
-  // }, [filters.fromDate]);
-
-  //   useEffect(() => {
-  //   if (moment(filters.toDate)?.isValid()) {
-  //     setEndDate(moment(filters.toDate)?.format("YYYY-MM-DD"));
-  //   } else {
-  //     setEndDate(moment.unix(filters.toDate)?.format("YYYY-MM-DD"));
-  //   }
-  // }, [filters.toDate]);
 
   const [isCalendarVisible, setCalendarVisible] = useState(false);
 
@@ -286,65 +216,6 @@ export default function FilterBar({
              endDate={endDate}
              formatWeekDay={nameOfDay => nameOfDay.substr(0, 1)}
           />
-          
-          {/* <div className={styles.date_time}>
-            <div className={styles.from_to}>
-              <div className={styles.from_to__header}>From</div>
-              <div
-                className={styles.from_to__datePicker}
-                onClick={() => setCalendarVisible(true)}
-              >
-                <div>
-                  {startDate
-                    ? startDate.toLocaleDateString("en-US")
-                    : "mm/dd/yyyy"}
-                  <i class="fa fa-calendar" aria-hidden="true"></i>
-                </div>
-              </div>
-            </div>
-            <div className={styles.from_to}>
-              <div className={styles.from_to__header}>To</div>
-              <div
-                className={styles.from_to__datePicker}
-                onClick={() => setCalendarVisible(true)}
-              >
-                <div>
-                  {endDate ? endDate.toLocaleDateString("en-US") : "mm/dd/yyyy"}
-                  <i class="fa fa-calendar" aria-hidden="true"></i>
-                </div>
-              </div>
-            </div>
-          </div> */}
-          {/* <div className={styles.last_day_container}>
-            <div className={styles.last_day_container__last_day}>
-              last 7 days
-            </div>
-            <div className={styles.last_day_container__last_day}>
-              last 14 days
-            </div>
-            <div className={styles.last_day_container__last_day}>
-              last 30 days
-            </div>
-            <div className={styles.last_day_container__last_day}>
-              last 90 days
-            </div>
-            <div className={styles.last_day_container__last_day}>last year</div>
-          </div>
-          {isCalendarVisible && (
-            <>
-              <DatePicker
-                selected={startDate}
-                onChange={onChange}
-                startDate={startDate}
-                endDate={endDate}
-                formatWeekDay={nameOfDay => nameOfDay.substr(0, 1)}
-                selectsRange
-                isCalendarVisible = {isCalendarVisible}
-                setCalendarVisible = {setCalendarVisible}
-                inline
-              />
-            </>
-          )} */}
         </div>
       </div>
       {showTagsModal && (
