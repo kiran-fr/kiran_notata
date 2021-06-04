ea8b7c54266a993f6522f939e5d9ee7a139947f0
atatonotata <jorgen@ataton.no>
2021-06-04 12:06:16 +0200

    working on funnels

--- src/private/NewDesign/settings/funnels.js
+++ src/private/NewDesign/settings/funnels.js
@@ -1,198 +1,332 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./funnels.scss";
-import { ICONPOSITION, SETTINGSMENU } from "../../NewDesign/srv_startup/pages/constants";
+import {
+  ICONPOSITION,
+  SETTINGSMENU,
+} from "../../NewDesign/srv_startup/pages/constants";
 import ButtonWithIcon from "../../NewDesign/srv_startup/pages/ui-kits/button-with-icon";
 import { Modal } from "../../../Components/UI_Kits/Modal/Modal";
+import { useQuery } from "@apollo/client";
+import { accountGet } from "../../Apollo/Queries";
+
+let colorArray = ["red", "blue", "purple", "orange", "green"];
+
+function getDefaultFunnel() {
+  return {
+    id: `${Math.floor(Math.random() * 1000).toString()}`,
+    name: "",
+    color: "red",
+    isNew: true,
+  };
+}
+
+function EditFunnel({ funnelGroup, save }) {
+  let [funnel, setFunnel] = useState({});
+  let [newFunnel, setNewFunnel] = useState(getDefaultFunnel());
+
+  useEffect(() => {
+    if (funnelGroup) {
+      setFunnel(funnelGroup);
+    }
+  }, [funnelGroup]);
+
+  function updateTag(tag) {
+    setFunnel({
+      ...funnel,
+      funnelTags: funnel.funnelTags.map(ft => (ft.id === tag.id ? tag : ft)),
+    });
+  }
+
+  return (
+    <div className="funnels-container__new-funnel">
+      <div className="funnels-container__new-funnel__text">
+        {funnel.name}
+        <ButtonWithIcon
+          className="funnels-container__new-funnel__save"
+          text="Save"
+          iconPosition={ICONPOSITION.NONE}
+          onClick={() => {
+            let saveObj = funnel;
+
+            if (newFunnel.name?.length) {
+              saveObj = {
+                ...funnel,
+                funnelTags: [...funnel.funnelTags, newFunnel],
+              };
+            }
+
+            console.log("save\n", JSON.stringify(saveObj, null, 2));
+          }}
+        />
+      </div>
+
+      <div className="groups">
+        {funnel.funnelTags?.map((tag, index) => {
+          return (
+            <div className="groups__group" key={tag.id}>
+              <div className="groups__group__heading">
+                <input
+                  type="text"
+                  maxLength={20}
+                  value={tag.name}
+                  onChange={e => {
+                    updateTag({ ...tag, name: e.target.value });
+                  }}
+                />
+              </div>
+              <div
+                className={`groups__group__funnel ${
+                  tag.color || colorArray[index] || "red"
+                }`}
+              />
+
+              <div className="groups__group__color-pallets">
+                {colorArray.map(color => (
+                  <div
+                    key={color}
+                    className={`color-pallet ${color} ${
+                      tag.color === color ? "selected" : ""
+                    }`}
+                    onClick={() => updateTag({ ...tag, color })}
+                  />
+                ))}
+              </div>
+
+              <i
+                className="fa fa-times-circle"
+                aria-hidden="true"
+                onClick={() => {
+                  setFunnel({
+                    ...funnel,
+                    funnelTags: funnel.funnelTags.filter(
+                      ({ id }) => id !== tag.id
+                    ),
+                  });
+                }}
+              />
+            </div>
+          );
+        })}
+
+        <div className="groups__group">
+          <div className="groups__group__heading">
+            <input
+              type="text"
+              maxLength={20}
+              value={newFunnel.name}
+              onChange={e => {
+                setNewFunnel({
+                  ...newFunnel,
+                  name: e.target.value,
+                });
+              }}
+            />
+          </div>
+          <div
+            className={`groups__group__funnel ${newFunnel.color || "red"}`}
+          />
+
+          <div className="groups__group__color-pallets">
+            {colorArray.map(color => (
+              <div
+                key={color}
+                className={`color-pallet ${color} ${
+                  newFunnel.color === color ? "selected" : ""
+                }`}
+                onClick={() => {
+                  setNewFunnel({
+                    ...newFunnel,
+                    color,
+                  });
+                }}
+              />
+            ))}
+          </div>
+
+          <i
+            className="fa fa-plus-circle"
+            aria-hidden="true"
+            onClick={() => {
+              if (!newFunnel.name.length) {
+                return;
+              }
+              setFunnel({
+                ...funnel,
+                funnelTags: [...funnel.funnelTags, newFunnel],
+              });
+              setNewFunnel(getDefaultFunnel());
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Funnel({ funnelGroup, setEdit }) {
+  const [openDropdown, setOpenDropdown] = useState(false);
+
+  return (
+    <div className="funnels-container__funnels__funnel">
+      <div className="funnels-container__funnels__funnel__heading">
+        {funnelGroup.name}
+        <span
+          className="material-icons tags-container__tag__heading__more"
+          onClick={() => setOpenDropdown(!openDropdown)}
+        >
+          {" "}
+          more_horiz{" "}
+          {openDropdown && (
+            <div className="funnels-container__funnels__funnel__heading__dropdown">
+              <div
+                className="drop-down-item"
+                onClick={() => setOpenDropdown(false)}
+              >
+                <span className="material-icons">edit</span>
+                <span
+                  className="text"
+                  onClick={() => {
+                    setEdit(funnelGroup.id);
+                  }}
+                >
+                  EDIT
+                </span>
+              </div>
+              <div
+                className="drop-down-item leave"
+                onClick={() => setOpenDropdown(false)}
+              >
+                <span className="material-icons leave">delete</span>
+                <span
+                  className="text"
+                  onClick={() => {
+                    console.log("delete funnel group");
+                  }}
+                >
+                  DELETE FUNNEL
+                </span>
+              </div>
+            </div>
+          )}
+        </span>
+      </div>
+
+      <div className="parts">
+        {funnelGroup.funnelTags?.map((tag, index) => {
+          // Max width of bar
+          let max = 180;
+
+          // Min width of bar
+          let min = 20;
+
+          // Difference
+          let diff = max - min;
+
+          // Increments
+          let inc = Math.round(diff / funnelGroup.funnelTags.length);
+
+          // Max with - increment multiplied by index
+          let width = `${max - inc * index}px`;
+
+          return (
+            <div className="parts__part" key={tag.id}>
+              <div className="parts__part__heading">{tag.name}</div>
+              <div
+                className={`parts__part__funnel ${
+                  tag.color || colorArray[index] || "red"
+                }`}
+                style={{ width }}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
 
 export default function Funnels({ setMenuSelected }) {
+  // States
   const [createModal, setCreateModal] = useState(false);
-  const [isFunnelDropDown, setIsFunnelDropDown] = useState(false);
+  // const [isFunnelDropDown, setIsFunnelDropDown] = useState(false);
   const [noOfFunnels, setNoOfFunnels] = useState(1);
   const [newFunnel, setNewFunnel] = useState("");
   const [selectedColorPallet, setSelectedColorPallet] = useState("red");
   const [newLevelText, setNewLevelText] = useState("");
   const [newFunnelLevels, setNewFunnelLevels] = useState([]);
+
+  const [isEditing, setIsEditing] = useState({});
+
+  const [funnels, setFunnels] = useState([]);
+
+  // Queries
+  const { data } = useQuery(accountGet);
+  let funnelGroups = data?.accountGet?.funnelGroups;
+
+  useEffect(() => {
+    if (funnelGroups) {
+      setFunnels(funnelGroups);
+    }
+  }, [funnelGroups]);
+
   return (
     <div className="funnels-container">
       <div className="card funnels-container__card">
+        {/* NAVIGATION */}
         <div className="card-heading funnels-container__heading">
           <i
-            class="fa fa-chevron-left"
+            className="fa fa-chevron-left"
             aria-hidden="true"
             onClick={() => setMenuSelected(SETTINGSMENU.HOME)}
-          ></i>
+          />
           Funnels
         </div>
+
+        {/* FUNNELS */}
         <div className="funnels-container__funnels">
-          {[...Array(noOfFunnels)].map((elementInArray, index) => {
-            return (
-              <div
-                className="funnels-container__funnels__funnel"
-                key={`funnel-record-id-${index}`}
-              >
-                <div className="funnels-container__funnels__funnel__heading">
-                  STAGE
-                  <span
-                    class="material-icons tags-container__tag__heading__more"
-                    onClick={() => setIsFunnelDropDown(!isFunnelDropDown)}
-                  >
-                    {" "}
-                    more_horiz{" "}
-                    {isFunnelDropDown && (
-                      <div className="funnels-container__funnels__funnel__heading__dropdown">
-                        <div
-                          className="drop-down-item"
-                          onClick={() => setIsFunnelDropDown(false)}
-                        >
-                          <span class="material-icons">edit</span>
-                          <span className="text">EDIT</span>
-                        </div>
-                        <div
-                          className="drop-down-item leave"
-                          onClick={() => setIsFunnelDropDown(false)}
-                        >
-                          <span class="material-icons leave">delete</span>
-                          <span className="text">DELETE FUNNEL</span>
-                        </div>
-                      </div>
-                    )}
-                  </span>
-                </div>
-                <div className="parts">
-                  <div className="parts__part">
-                    <div className="parts__part__heading">Reviewed</div>
-                    <div className="parts__part__funnel red"></div>
-                  </div>
-                  <div className="parts__part">
-                    <div className="parts__part__heading">Met</div>
-                    <div className="parts__part__funnel blue"></div>
-                  </div>
-                  <div className="parts__part">
-                    <div className="parts__part__heading">Analyzed</div>
-                    <div className="parts__part__funnel purple"></div>
-                  </div>
-                  <div className="parts__part">
-                    <div className="parts__part__heading">IC</div>
-                    <div className="parts__part__funnel orange"></div>
-                  </div>
-                  <div className="parts__part">
-                    <div className="parts__part__heading">Invested</div>
-                    <div className="parts__part__funnel green"></div>
-                  </div>
-                </div>
-              </div>
-            );
+          {funnels.map(funnelGroup => {
+            if (!isEditing[funnelGroup.id]) {
+              return (
+                <Funnel
+                  key={funnelGroup.id}
+                  funnelGroup={funnelGroup}
+                  setEdit={() => {
+                    setIsEditing({
+                      ...isEditing,
+                      [funnelGroup.id]: true,
+                    });
+                  }}
+                />
+              );
+            }
+
+            if (isEditing[funnelGroup.id]) {
+              return (
+                <EditFunnel
+                  key={funnelGroup.id}
+                  funnelGroup={funnelGroup}
+                  save={() => {
+                    setIsEditing({
+                      ...isEditing,
+                      [funnelGroup.id]: false,
+                    });
+                  }}
+                />
+              );
+            }
           })}
         </div>
-        {newFunnel && (
-          <div className="funnels-container__new-funnel">
-            <div className="funnels-container__new-funnel__text">
-              {newFunnel}
-              <ButtonWithIcon
-                className="funnels-container__new-funnel__save"
-                text="Save"
-                iconPosition={ICONPOSITION.NONE}
-                onClick={() => {
-                  setNoOfFunnels(noOfFunnels + 1);
-                  setNewFunnelLevels([]);
-                  setNewLevelText("");
-                  setNewFunnel("");
-                }}
-              ></ButtonWithIcon>
-            </div>
-            <div className="groups">
-              {newFunnelLevels.map((item, index) => {
-                return (
-                  <div className="groups__group">
-                    <div className="groups__group__heading-text">
-                      {item.levelName}
-                    </div>
-                    <div
-                      className={`groups__group__funnel ${item.selectedColor}`}
-                    ></div>
-                    <i
-                      class="fa fa-times-circle"
-                      aria-hidden="true"
-                      onClick={() => {
-                        let newFunnels = [...newFunnelLevels];
-                        newFunnels.splice(index, 1);
-                        setNewFunnelLevels(newFunnels);
-                      }}
-                    ></i>
-                  </div>
-                );
-              })}
-              <div className="groups__group">
-                <div className="groups__group__heading">
-                  <input
-                    type="text"
-                    maxLength={10}
-                    value={newLevelText}
-                    onChange={e => setNewLevelText(e.target.value)}
-                  />
-                </div>
-                <div
-                  className={`groups__group__funnel ${selectedColorPallet}`}
-                ></div>
-                <div className="groups__group__color-pallets">
-                  <div
-                    className={`color-pallet red ${
-                      selectedColorPallet === "red" ? "selected" : ""
-                    }`}
-                    onClick={() => setSelectedColorPallet("red")}
-                  ></div>
-                  <div
-                    className={`color-pallet blue ${
-                      selectedColorPallet === "blue" ? "selected" : ""
-                    }`}
-                    onClick={() => setSelectedColorPallet("blue")}
-                  ></div>
-                  <div
-                    className={`color-pallet purple ${
-                      selectedColorPallet === "purple" ? "selected" : ""
-                    }`}
-                    onClick={() => setSelectedColorPallet("purple")}
-                  ></div>
-                  <div
-                    className={`color-pallet orange ${
-                      selectedColorPallet === "orange" ? "selected" : ""
-                    }`}
-                    onClick={() => setSelectedColorPallet("orange")}
-                  ></div>
-                  <div
-                    className={`color-pallet green ${
-                      selectedColorPallet === "green" ? "selected" : ""
-                    }`}
-                    onClick={() => setSelectedColorPallet("green")}
-                  ></div>
-                </div>
-                <i
-                  class="fa fa-plus-circle"
-                  aria-hidden="true"
-                  onClick={() => {
-                    if (newLevelText) {
-                      let newFunnels = [...newFunnelLevels];
-                      newFunnels.push({
-                        levelName: newLevelText,
-                        selectedColor: selectedColorPallet,
-                      });
-                      setNewFunnelLevels(newFunnels);
-                      setNewLevelText("");
-                      setSelectedColorPallet("red");
-                    }
-                  }}
-                ></i>
-              </div>
-            </div>
-          </div>
-        )}
+
+        {/* NEW FUNNEL */}
+        {newFunnel && <EditFunnel />}
+
         <ButtonWithIcon
           className="funnels-container__create-new-funnel"
           iconName="add"
           text="CREATE NEW GROUP"
           iconPosition={ICONPOSITION.START}
           onClick={() => setCreateModal(true)}
-        ></ButtonWithIcon>
+        />
       </div>
       {createModal && (
         <Modal
@@ -214,9 +348,9 @@ export default function Funnels({ setMenuSelected }) {
               type="text"
               value={newFunnel}
               onChange={e => setNewFunnel(e.target.value)}
-            ></input>
+            />
           }
-        ></Modal>
+        />
       )}
     </div>
   );
--- src/private/NewDesign/settings/web-form.js
+++ src/private/NewDesign/settings/web-form.js
@@ -1,19 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./web-form.scss";
-import { ICONPOSITION, SETTINGSMENU } from "../../NewDesign/srv_startup/pages/constants";
+import {
+  ICONPOSITION,
+  SETTINGSMENU,
+} from "../../NewDesign/srv_startup/pages/constants";
 import copy from "../../../assets/images/copy.png";
 import ButtonWithIcon from "../../NewDesign/srv_startup/pages/ui-kits/button-with-icon";
+import { useQuery } from "@apollo/client";
+import { accountGet } from "../../Apollo/Queries";
 
 export default function WebForm({ setMenuSelected }) {
+  // States
+  const [copyUrlSuccess, setCopyUrlSuccess] = useState(false);
+  const [copyIframeSuccess, setCopyIframeSuccess] = useState(false);
+
+  // Queries
+  const accountQuery = useQuery(accountGet);
+
+  // Data maps
+  let account = accountQuery.data?.accountGet || {};
+
+  const iFrameUrl =
+    `${window.location.protocol}//` +
+    `${window.location.host}/` +
+    `public/` +
+    `${account.id || "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"}/` +
+    `form.html`;
+
+  const iFrameContent = `<iframe src="${iFrameUrl}" style="position: absolute; top: 0; left: 0; bottom: 0; right: 0; width: 100%; height: 100%; border: none;"></iframe>`;
+
+  function copyIframeToClipboard() {
+    navigator.clipboard.writeText(iFrameContent);
+    setCopyIframeSuccess(true);
+    setCopyUrlSuccess(false);
+  }
+
+  function copyUrlToClipboard() {
+    navigator.clipboard.writeText(iFrameUrl);
+    setCopyIframeSuccess(false);
+    setCopyUrlSuccess(true);
+  }
+
   return (
     <div className="web-form-container">
       <div className="card web-form-container__card">
         <div className="card-heading web-form-container__heading">
           <i
-            class="fa fa-chevron-left"
+            className="fa fa-chevron-left"
             aria-hidden="true"
             onClick={() => setMenuSelected(SETTINGSMENU.HOME)}
-          ></i>
+          />
           Web Form
         </div>
         <div className="web-form">
@@ -22,18 +58,16 @@ export default function WebForm({ setMenuSelected }) {
             embeddable code below, or refer to the link.
           </p>
           <div className="web-form__link">Link</div>
-          <p className="web-form__link-url">
-            https://d1nLcbGO4xMlamugkR2OuCnjGgU_eU1yduJSdWYDE7
-          </p>
-          <div className="web-form__copy-link">
-            <img src={copy}></img> Copy Link
+          <p className="web-form__link-url">{iFrameUrl}</p>
+          <div className="web-form__copy-link" onClick={copyUrlToClipboard}>
+            <img src={copy} />
+            {copyUrlSuccess ? "Link copied" : "Copy Link"}
           </div>
           <div className="web-form__code-heading">CODE</div>
-          <div className="web-form__code-iframe">
-            {`<iframe src="https://www.notata.io/public/b2d2c1b6-5a40-10d8-3780-4dbef72ce170/form.html" style="position: absolute; top: 0; left: 0; bottom: 0; right: 0; width: 100%; height: 100%; border: none;"></iframe>`}
-          </div>
-          <div className="web-form__copy-link">
-            <img src={copy}></img> Copy code
+          <div className="web-form__code-iframe">{iFrameContent}</div>
+          <div className="web-form__copy-link" onClick={copyIframeToClipboard}>
+            <img src={copy} />
+            {copyIframeSuccess ? "Code copied" : "Copy code"}
           </div>
           <div className="web-form__customization-container">
             <ButtonWithIcon
@@ -41,7 +75,7 @@ export default function WebForm({ setMenuSelected }) {
               iconName="add"
               text="Customize form"
               iconPosition={ICONPOSITION.NONE}
-            ></ButtonWithIcon>
+            />
           </div>
         </div>
       </div>
