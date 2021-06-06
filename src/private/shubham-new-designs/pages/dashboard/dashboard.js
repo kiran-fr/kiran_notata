f7b19cd31605761a716107def031b3a96d5cbc2a
sgoyal2243 <shubhamgoyal2243@gmail.com>
2021-06-06 16:28:42 +0530

    dashbaord changes

--- src/private/shubham-new-designs/pages/dashboard/dashboard.js
+++ src/private/shubham-new-designs/pages/dashboard/dashboard.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./dashboard.scss";
 import notificationProfile from "../../../../assets/images/dashboard-notifictaion-profile.png";
 import notificationConversation from "../../../../assets/images/dashboard-notifictaion-converstation.png";
+import ReportAnalyticChart from "./report-analytics-chart";
+import ReportAnalyticFunnel from "./report-analytics-funnel";
 
 export default function DashboardNew() {
   return (
@@ -305,6 +307,71 @@ export default function DashboardNew() {
             </div>
             <div className="dashboard-container__notification__more-updates">
               MORE udpates <i className="fa fa-chevron-down"></i>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-5 col-md-12 col-sm-12 col-xs-12">
+          <div className="card dashboard-container__reports-and-analytics">
+            <div className="dashboard-container__reports-and-analytics__heading">
+              Reports and analytics
+            </div>
+            <div className="dashboard-container__reports-and-analytics__scores-container">
+              <div className="dashboard-container__reports-and-analytics__scores-container__score">
+                <div className="dashboard-container__reports-and-analytics__scores-container__score__heading">
+                  All startups
+                </div>
+                <div className="dashboard-container__reports-and-analytics__scores-container__score__value-container">
+                  <div className="dashboard-container__reports-and-analytics__scores-container__score__value-container__value">
+                    1825
+                  </div>
+                  <div className="dashboard-container__reports-and-analytics__scores-container__score__value-container__performance">
+                    <div className="dashboard-container__reports-and-analytics__scores-container__score__value-container__performance__score">
+                      +83
+                    </div>
+                    <div className="dashboard-container__reports-and-analytics__scores-container__score__value-container__performance__indicator">
+                      <span class="dashboard-container__reports-and-analytics__scores-container__score__value-container__performance__indicator--up-arrow material-icons">
+                        north
+                      </span>
+                      <span className="dashboard-container__reports-and-analytics__scores-container__score__value-container__performance__indicator--since-last">
+                        since last
+                        <br /> month
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="dashboard-container__reports-and-analytics__scores-container__score">
+                <div className="dashboard-container__reports-and-analytics__scores-container__score__heading">
+                  People in your network
+                </div>
+                <div className="dashboard-container__reports-and-analytics__scores-container__score__value-container">
+                  <div className="dashboard-container__reports-and-analytics__scores-container__score__value-container__value">
+                    532
+                  </div>
+                  <div className="dashboard-container__reports-and-analytics__scores-container__score__value-container__performance">
+                    <div className="dashboard-container__reports-and-analytics__scores-container__score__value-container__performance__score">
+                      +24
+                    </div>
+                    <div className="dashboard-container__reports-and-analytics__scores-container__score__value-container__performance__indicator">
+                      <span class="dashboard-container__reports-and-analytics__scores-container__score__value-container__performance__indicator--up-arrow material-icons">
+                        north
+                      </span>
+                      <span className="dashboard-container__reports-and-analytics__scores-container__score__value-container__performance__indicator--since-last">
+                        since last
+                        <br /> month
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="dashboard-container__reports-and-analytics__chart-funnel-container">
+              <div className="dashboard-container__reports-and-analytics__chart-funnel-container--chart">
+                <ReportAnalyticChart />
+              </div>
+              <div className="dashboard-container__reports-and-analytics__chart-funnel-container--chart">
+                <ReportAnalyticFunnel />
+              </div>
             </div>
           </div>
         </div>
