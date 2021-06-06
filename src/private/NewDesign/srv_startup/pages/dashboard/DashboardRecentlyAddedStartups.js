37f81f83cc06339a421efbffc4ec48c91e25d472
atatonotata <jorgen@ataton.no>
2021-06-06 23:05:37 +0200

    without empty states

--- src/private/NewDesign/srv_startup/pages/dashboard/DashboardRecentlyAddedStartups.js
+++ src/private/NewDesign/srv_startup/pages/dashboard/DashboardRecentlyAddedStartups.js
@@ -13,7 +13,7 @@ export default function DashboardRecentlyAddedStartups({ history }) {
     <div className="card dashboard-container__recently-added-card">
       <div className="card-heading-designer">Recently added startups</div>
 
-      {loading && <Loader />}
+      {!data && loading && <Loader />}
 
       <div className="dashboard-container__recently-added-card__data-container">
         {list.map(({ group, creative }) => (
