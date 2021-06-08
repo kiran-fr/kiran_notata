92f7657fc7561874f80f1a46c9daae867955dc27
shirisha Eppakayala <shirisha.eppakayala8@gmail.com>
2021-06-08 12:00:00 +0200

    settings page fix

--- src/private/NewDesign/settings/notifications.js
+++ src/private/NewDesign/settings/notifications.js
@@ -55,7 +55,7 @@ export default function Notifications({ history }) {
 
   return (
     <div className="notifications-container">
-      <div className=" notifications-container__card">
+      <div className="card notifications-container__card">
         <div className="card-heading notifications-container__header">
           <div className="card-heading notifications-container__heading">
             <i
@@ -65,7 +65,7 @@ export default function Notifications({ history }) {
             ></i>
             Notifications
           </div>
-          <div className="card-heading notifications-container__show">
+          <div className=" notifications-container__show">
             Show
             <i
               className={`fa ${
