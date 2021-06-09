0b67c330e39623fff4b574ae2abdd6b3fd52cc2b
siva <sivakumarjegadesan@gmail.com>
2021-06-10 01:19:05 +0530

    group logic.

--- src/Containers/NewDesign/PreProfile/Page3.js
+++ src/Containers/NewDesign/PreProfile/Page3.js
@@ -1,73 +1,17 @@
-/* Date : 10/04/2020 */
-/* Created By : siva */
-
 import React from "react";
-
 // REACT STUFF
 import { useForm } from "react-hook-form";
-
 import { startup_page } from "definitions.js";
-
 import styles from "./Profile.module.css";
 import Group from "./group/group";
 import { Button } from "Components/UI_Kits";
-
 export default function Page3({ setPage, extraInputs, history }) {
   const { register, handleSubmit, formState } = useForm();
   const { isSubmitting } = formState;
-
   const onSubmit = async (data, event) => {
     event.preventDefault();
-
     history.push(startup_page);
   };
-
-  const data = [
-    {
-      title: "business angels 1",
-      admin: "Stephanie Wykoff",
-      group: {
-        id: "123",
-        name: "Group Name",
-        groupDescription: "Group description",
-        createdByUser: {
-          family_name: "Tushar",
-          given_name: "Kumar",
-          email: "jorgen@notata.io",
-        },
-        members: [],
-      },
-    },
-    {
-      title: "business angels 2",
-      admin: "Stephanie Wykoff",
-      group: {
-        groupId: "23234234",
-        groupName: "Group Name",
-        groupDescription: "Group description",
-        createdByUser: {
-          family_name: "Ankit",
-          given_name: "Kumar",
-          email: "jorgen@notata.io",
-        },
-      },
-    },
-    {
-      title: "business angels 3",
-      admin: "Stephanie Wykoff",
-      group: {
-        groupId: "12312141",
-        groupName: "Group Name",
-        groupDescription: "Group description",
-        createdByUser: {
-          family_name: "Saurav",
-          given_name: "Anand",
-          email: "jorgen@notata.io",
-        },
-      },
-    },
-  ];
-
   return (
     <form
       className="notata_form"
@@ -82,16 +26,7 @@ export default function Page3({ setPage, extraInputs, history }) {
         >
           Do you want to be a part of these groups?
         </h1>
-
-        {data.map((item, i) => (
-          <Group
-            key={i}
-            group={item.group}
-            id={item.id}
-            title={item.title}
-            admin={item.admin}
-          />
-        ))}
+          <Group/>
         <div className={styles.button_container_justify}>
           <Button
             size="medium"
