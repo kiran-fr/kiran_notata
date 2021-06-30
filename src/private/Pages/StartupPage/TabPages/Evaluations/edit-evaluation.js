1fccc6d0d5cb5ef884e5022c2a5114bd2412deb3
atatonotata <jorgen@ataton.no>
2021-06-30 16:44:44 +0200

    closes #399

--- src/private/Pages/StartupPage/TabPages/Evaluations/edit-evaluation.js
+++ src/private/Pages/StartupPage/TabPages/Evaluations/edit-evaluation.js
@@ -19,6 +19,7 @@ export default function EditEvaluation({
   setActiveEvaluation,
   setEvaluateModal,
 }) {
+  // States
   const [answers, setAnswers] = useState([]);
   const [loading, setLoading] = useState(false);
 
